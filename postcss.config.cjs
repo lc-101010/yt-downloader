d2c09957b2a03bd0852b6ea34242748a4c3d874e
lc-101010 <lcommander101010@gmail.com>
2024-11-02 23:52:55 -0500

    catppuccin added

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,7 +2,15 @@ module.exports = {
   plugins: {
     tailwindcss: {
       content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
-      // ...other options
+      plugins: [
+        require("@catppuccin/tailwindcss")({
+          // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
+          // default is `false`, which means no prefix
+          prefix: "ctp",
+          // which flavour of colours to use by default, in the `:root`
+          defaultFlavour: "latte",
+        }),
+      ],
     },
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="content">
-      <h1 className="flex p-2 text-red-400">Rsbuild with React</h1>
+      <h1 className="text-ctp-mauve">Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
