881e212822361a7fd9cbff9a5167e91bd3b92efb
lc-101010 <lcommander101010@gmail.com>
2024-11-05 05:49:02 -0500

    remove all files and reconfigure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div className="flex flex-col">
-      <Navbar />
-      <div className="bg-red-200">
-        <h1 className="text-ctp-mauve">Rsbuild with React</h1>
-        <p className="dark:text-ctp-red">
-          Start building amazing things with Rsbuild.
-        </p>
-      </div>
+    <div className="content">
+      <h1 className='text-ctp-red'>Rsbuild with React</h1>
+      <h1 className="font-bold text-3xl underline">Hello world!</h1>
+      <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
 };
